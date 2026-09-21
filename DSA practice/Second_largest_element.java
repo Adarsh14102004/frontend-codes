0d3e80c521465f4f362c9a2d185ab3a395172496
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-09-21 19:22:30 +0530

    Second largest element in Array

--- DSA practice/Second_largest_element.java
+++ DSA practice/Second_largest_element.java
@@ -3,7 +3,7 @@ public class Second_largest_element {
     public static void main(String args[]) {
         int largest = Integer.MIN_VALUE;
         int second = Integer.MIN_VALUE;
-        int arr[] = {1, 2, 3, 4, 5,6,7};
+        int arr[] = {1, 2, 3, 4, 5,6,7,8,9};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > largest) {
                 second = largest;
