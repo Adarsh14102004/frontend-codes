62f66ffb2be7c62a4241eb386cf4988072be816e
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-09-25 13:48:31 +0530

    reverse Array

--- DSA practice/Reverse_Array.java
+++ DSA practice/Reverse_Array.java
@@ -1,6 +1,6 @@
 class Reverse_Array {
     public static void main(String args[]) {
-        int arr[] = { 1, 2, 3, 4, 5,6,7};
+        int arr[] = { 1, 2, 3, 4, 5,6,7,8,9};
         int i = 0;
         int j = arr.length - 1;
         while (i < j) {
@@ -13,6 +13,7 @@ class Reverse_Array {
         for (int k = 0; k < arr.length; k++) {
             System.out.print(arr[k] + " ");
         }
+        
     }
 
 }
